f1844eefd44af75ffbf8dc1f8f217527f9b88c6e
ahmadesmaili <ahmadesmaili71@gmail.com>
2023-05-23 00:23:28 +0330

    creat app bar mobile section And filter & sort mobile section And product section And loop in product by data

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
     content: ["./public/**/*.html"],
     theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+        },
         extend: {
             fontFamily: {
                 'sans': ['IRANSansX']
